0525f7bda94f3d2866d5ca50038b5d45597a42e6
piotrkoson <piotr@koson.pl>
2017-07-18 19:38:30 +0200

    Add root reducer and saga files

---
+++ src/saga/index.js
@@ -0,0 +1,11 @@
+// @flow
+import { fork, takeLatest, put, call, select, all } from 'redux-saga/effects';
+
+// workers
+
+// watchers
+
+// rootSaga
+function* rootSaga(): Generator<Array<() => void>, void, *> {}
+
+export default rootSaga;
