cddad527ae10651c62eb9595c38c331872f75ce1
piotrkoson <piotr@koson.pl>
2017-07-25 23:44:17 +0200

    Add styles for button

--- src/scenes/Home/styled.js
+++ src/scenes/Home/styled.js
@@ -4,3 +4,12 @@ export const MapBox = styled.div`
   width: 100%;
   height: 500px;
 `;
+
+export const Button = styled.button`
+  padding: 8px;
+  background: #00d0ff;
+  border: 0;
+  border-radius: 4px;
+  font-weight: bold;
+  cursor: pointer;
+`;
