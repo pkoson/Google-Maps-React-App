d73ac2a2647cf1154574f6d9a47794f373304e23
piotrkoson <piotr@koson.pl>
2017-07-25 21:08:23 +0200

    Add addMarkerToMap function

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -2,15 +2,18 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Map, List } from 'immutable';
 
 import { mapActionsToProps } from '../../utils';
 import { MapBox } from './styled';
+
+import type { StateType } from './reducer';
 
 const google = window.google;
 
 export class Home extends Component {
   map: HTMLDivElement;
-  map = null;
+  GoogleMap: any;
   componentDidMount = () => {
     this.initMap();
   };
@@ -18,16 +21,27 @@ export class Home extends Component {
   initMap = () => {
     // TODO: get it from initial state
     const startPointer = { lng: 19.93658, lat: 50.06143 };
-    const map = new google.maps.Map(this.map, {
+    this.GoogleMap = new google.maps.Map(this.map, {
       zoom: 14,
       center: startPointer
     });
     // add onClick event listener
-    google.maps.event.addListener(map, 'click', event => {
-      this.props.actions.onMapClicked(event.latLng, map);
+    google.maps.event.addListener(this.GoogleMap, 'click', event => {
+      this.props.actions.onMapClicked(event.latLng, this.GoogleMap);
     });
   };
-  componentDidUpdate = () => {};
+
+  addMarkerToMap = (marker: List<*>, map: any) => {
+    const newMarker = new google.maps.Marker({
+      position: { lat: marker.last().lat(), lng: marker.last().lng() },
+      map
+    });
+  };
+  componentDidUpdate = (prevProps: { Home: StateType }) => {
+    const markersProp = this.props.Home.get('markers');
+    if (prevProps.Home.get('markers').size !== markersProp)
+      this.addMarkerToMap(markersProp, this.GoogleMap);
+  };
   render() {
     return (
       <MapBox
